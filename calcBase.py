743375fa4768e3490166fff8f508df99ff73c670
Charles <charlescretois@gmail.com>
2022-02-01 10:13:56 +0100

    Test

--- calcBase.py
+++ calcBase.py
@@ -3,7 +3,7 @@
 #
 # Expressions arithmétiques sans variables
 # -----------------------------------------------------------------------------
-
+#test
 from genereTreeGraphviz2 import printTreeGraph
 
 tokens = [
@@ -81,7 +81,7 @@ def p_start(p):
     print('Arbre de dérivation = ',p[0])
     printTreeGraph(p[1])
     evalInst(p[1])
-    
+
 def p_bloc(p):
     '''bloc : bloc statement SEMICOLON
                             | statement SEMICOLON'''
@@ -89,7 +89,7 @@ def p_bloc(p):
         p[0] = ('bloc', p[1], 'empty')
     else:
         p[0] = ('bloc', p[1], p[2])
-    
+
 def p_statement_expr(p):
     'statement : expression'
     p[0] = p[1]
@@ -119,7 +119,7 @@ def p_expression_binop_bool2(p):
 
     p[0] = (p[2], p[1], p[3])
 
-    
+
 def p_expression_binop_bool(p):
     '''expression : expression AND expression
                                 | expression OR expression'''
@@ -127,7 +127,7 @@ def p_expression_binop_bool(p):
         p[0] = ('and', p[1], p[3])
     else :
         p[0] = ('or', p[1], p[3])
-    
+
 def p_expression_binop_times(p):
     'expression : expression TIMES expression'
     p[0] = ('*', p[1], p[3])
@@ -136,7 +136,7 @@ def p_expression_binop_divide_and_minus(p):
     '''expression : expression MINUS expression
 				| expression DIVIDE expression'''
     p[0] = (p[2], p[1], p[3])
-	
+
 def p_expression_group(p):
     'expression : LPAREN expression RPAREN'
     p[0] = p[2]
@@ -188,7 +188,7 @@ def evalExpr(t):
             return evalExpr(t[1]) > evalExpr(t[2])
         elif t[0] == "<":
             return evalExpr(t[1]) < evalExpr(t[2])
-        
+
 def evalInst(t):
     if t[0] == "bloc":
         #systeme de pile (mettre sur la pile)
@@ -204,11 +204,9 @@ def evalInst(t):
     elif t[0] == "while":
         while evalExpr(t[1]):
             evalInst(t[2])
-            
+
 import ply.yacc as yacc
 yacc.yacc()
 
 s = 'x=4; if x<8 { print(true & false); };'
 yacc.parse(s)
-
-    
